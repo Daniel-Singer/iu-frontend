0f9a1c09e04471e5e9646aac25840ef3242206ad
Daniel <singer.daniel85@gmail.com>
2024-06-09 23:50:53 +0200

    Navlink made reuseable

--- src/components/navlink/Navlink.tsx
+++ src/components/navlink/Navlink.tsx
@@ -1,5 +1,13 @@
-const Navlink = () => {
-  return <>Navlink</>;
+import { UnstyledButton } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+
+const Navlink = ({ label, path }: INavlink) => {
+  const navigate = useNavigate();
+  return (
+    <UnstyledButton onClick={() => navigate(`/${path}`)}>
+      {label}
+    </UnstyledButton>
+  );
 };
 
 export default Navlink;
---
+++ src/types/Navlink.d.ts
@@ -0,0 +1,4 @@
+interface INavlink {
+  path: string;
+  label: string;
+}
