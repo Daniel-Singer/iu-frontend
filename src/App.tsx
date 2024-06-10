94c72adbd22e76a580daa42cdf086840b8518282
Daniel <singer.daniel85@gmail.com>
2024-06-10 08:58:30 +0200

    init commit routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import { MantineProvider } from "@mantine/core";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { MantineProvider } from '@mantine/core';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import "@mantine/core/styles.css";
-import "./App.css";
-import DashboardScreen from "./screens/DashboardScreen";
-import MessagesScreen from "./screens/MessageesScreen";
-import NotFoundScreen from "./screens/NotFoundScreen";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DashboardScreen />,
-  },
-  {
-    path: "/messages",
-    element: <MessagesScreen />,
-  },
-  {
-    path: "/*",
-    element: <NotFoundScreen />,
-  },
-]);
+import '@mantine/core/styles.css';
+import './App.css';
+import DashboardScreen from './screens/DashboardScreen';
+import MessagesScreen from './screens/MessageesScreen';
+import NotFoundScreen from './screens/NotFoundScreen';
+import ScreenLayout from './layout/ScreenLayout';
 
 const App = () => {
   return (
     <MantineProvider>
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ScreenLayout />}>
+            <Route index element={<DashboardScreen />} />
+            <Route path="/messages" element={<MessagesScreen />} />
+          </Route>
+          <Route path="/*" element={<NotFoundScreen />} />
+        </Routes>
+      </Router>
     </MantineProvider>
   );
 };
