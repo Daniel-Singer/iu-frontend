389a69b3fdccf716f0fbfa88525fff5b4308994f
Daniel <singer.daniel85@gmail.com>
2024-06-10 14:06:19 +0200

    init commit issues mock data

---
+++ src/screens/IssueDetailsScreen.tsx
@@ -0,0 +1,11 @@
+import ScreenHeader from '../components/screen/ScreenHeader';
+
+const IssueDetailsScreen = () => {
+  return (
+    <>
+      <ScreenHeader>Fehlermeldung</ScreenHeader>
+    </>
+  );
+};
+
+export default IssueDetailsScreen;
