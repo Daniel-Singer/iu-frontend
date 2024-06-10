ebad89c33ec3b37c5ea99ca8915b0f63b9fc9800
Daniel <singer.daniel85@gmail.com>
2024-06-10 14:36:29 +0200

    added mock data for students

---
+++ src/screens/StudentsScreen.tsx
@@ -0,0 +1,13 @@
+import ScreenHeader from '../components/screen/ScreenHeader';
+
+import students from '../mock/students.json';
+
+const StudentsScreen = () => {
+  return (
+    <>
+      <ScreenHeader>Studentenübersicht</ScreenHeader>
+    </>
+  );
+};
+
+export default StudentsScreen;
