ffda4435f3f8b2b6bd798575983d48441c369c12
Daniel <singer.daniel85@gmail.com>
2024-06-25 08:44:35 +0200

    categories can be chosen

--- src/forms/issue/IssueForm.tsx
+++ src/forms/issue/IssueForm.tsx
@@ -1,10 +1,22 @@
 import { Select, Stack, TextInput, Textarea } from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
+import { listCategories } from '../../queries/categories/listCategories';
 
 const IssueForm = () => {
+  const { data: categories } = useQuery({
+    queryKey: ['categories'],
+    queryFn: listCategories,
+    select: (data) => {
+      return data?.map(({ id, label }) => ({
+        value: String(id!),
+        label,
+      }));
+    },
+  });
   return (
     <form>
       <Stack>
-        <Select label="Kategorie" data={[]} withAsterisk />
+        <Select label="Kategorie" data={categories} withAsterisk />
         <TextInput label="Kurzbeschreibung" withAsterisk />
         <Textarea label="Beschreibung" withAsterisk minRows={5} autosize />
       </Stack>
