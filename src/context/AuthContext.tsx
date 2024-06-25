74f534529ab8304251206f6eb1829f126d785cbf
Daniel <singer.daniel85@gmail.com>
2024-06-25 09:04:08 +0200

    authContext added

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { useSessionStorage } from '@mantine/hooks';
+import { ReactNode, createContext, useContext } from 'react';
+
+interface IAuthContext {
+  auth: any;
+  setAuth: any;
+}
+
+export const AuthContext = createContext<IAuthContext | undefined>(undefined);
+
+interface IAuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuhProvider = ({ children }: IAuthProviderProps) => {
+  const [auth, setAuth] = useSessionStorage({
+    key: 'auth',
+    defaultValue: undefined,
+  });
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error(
+      'useAuthContext muss sich innerhalb von AuthProvider befinden'
+    );
+  }
+  return context;
+};
