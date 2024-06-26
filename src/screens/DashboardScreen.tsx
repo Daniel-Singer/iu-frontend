2e13c4252b69ea7241130af1495d63b2eb4294de
Daniel <singer.daniel85@gmail.com>
2024-06-26 15:24:19 +0200

    searchbar added to dashboard screen

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -3,6 +3,7 @@ import AddButton from '../components/buttons/AddButton';
 import IssueModal from '../modals/issues/IssueModal';
 import IssuesTable from '../tables/issues/IssuesTable';
 import { useModalContext } from '../context/ModalContext';
+import SearchBar from '../components/search/SearchBar';
 
 const DashboardScreen = () => {
   const { open, toggleModal } = useModalContext();
@@ -10,7 +11,8 @@ const DashboardScreen = () => {
     <>
       <IssueModal open={open} onClose={toggleModal} />
       <ScreenHeader label="Dashboard">
-        <AddButton onClick={toggleModal}>Anlegen</AddButton>
+        <SearchBar />
+        <AddButton onClick={toggleModal}>Neue Meldung</AddButton>
       </ScreenHeader>
       <IssuesTable />
     </>
