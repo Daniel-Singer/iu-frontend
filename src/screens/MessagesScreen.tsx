64e43c0af94ac6490f1bc1dd1bb52563ade76978
Daniel <singer.daniel85@gmail.com>
2024-08-08 11:09:35 +0200

    todos hinzugefügt

--- src/screens/MessagesScreen.tsx
+++ src/screens/MessagesScreen.tsx
@@ -1,6 +1,7 @@
 import { Box, Grid } from '@mantine/core';
 import ScreenHeader from '../components/screen/ScreenHeader';
 import ContactsTable from '../tables/messages/ContactsTable';
+import MessageContainer from '../layout/messages/MessageContainer';
 
 // TODO - Design des Nachrichten Screens mithilfe von Komponenten
 // TODO - Implementierung der Abfrage an Server für neue Nachrichten
@@ -10,9 +11,12 @@ const MessagesScreen = () => {
     <>
       <ScreenHeader label="Nachrichten"></ScreenHeader>
       <Box flex={1}>
-        <Grid>
+        <Grid style={{ height: '100%' }}>
           <Grid.Col span={3}>
             <ContactsTable />
+          </Grid.Col>
+          <Grid.Col span={9}>
+            <MessageContainer />
           </Grid.Col>
         </Grid>
       </Box>
