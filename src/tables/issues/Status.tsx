8bda1b1686f5a0b656745d44834d9be8cceb5439
Daniel <singer.daniel85@gmail.com>
2024-08-08 17:59:10 +0200

    removed unnecessary imports in status

--- src/tables/issues/Status.tsx
+++ src/tables/issues/Status.tsx
@@ -1,22 +1,10 @@
-import { MantineColor, Table, Text } from '@mantine/core';
+import { Table } from '@mantine/core';
 import StatusLabel from '../../components/status/StatusLabel';
 
 interface IProps {
   id: number;
   label: string;
 }
-
-interface IColorMap {
-  [key: number]: MantineColor;
-}
-
-const colorMap: IColorMap = {
-  1: 'blue',
-  2: 'grape',
-  3: 'green',
-  4: 'red',
-  5: 'orange',
-};
 
 const Status = ({ id, label }: IProps) => {
   return (
