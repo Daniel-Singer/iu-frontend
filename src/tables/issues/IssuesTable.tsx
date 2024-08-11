9be00652900b92c754923b774f0d62dc0b9ba998
Daniel <singer.daniel85@gmail.com>
2024-08-11 20:32:51 +0200

    issues can be filtered

--- src/tables/issues/IssuesTable.tsx
+++ src/tables/issues/IssuesTable.tsx
@@ -5,11 +5,23 @@ import { listMyIssues } from '../../queries/issues/listMyIssues';
 import classes from './IssuesTable.module.css';
 import Thead from './Thead';
 import Tbody from './Tbody';
+import { useSearchContext } from '../../context/SearchContext';
 
 const IssuesTable = () => {
+  const { searchValue } = useSearchContext();
   const { data: myIssues } = useQuery({
     queryKey: ['my_issues'],
     queryFn: listMyIssues,
+    select: (issues) => {
+      if (!searchValue || searchValue === '') {
+        return issues;
+      }
+      return issues?.filter(
+        ({ course, category }) =>
+          course.code.toLowerCase().includes(searchValue?.toLowerCase()) ||
+          category.label.toLowerCase().includes(searchValue?.toLowerCase())
+      );
+    },
   });
   if (myIssues?.length! > 0) {
     return (
