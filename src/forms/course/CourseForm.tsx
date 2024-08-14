9a08953b6762b28b36cb891dfbdba356447b8ae5
Daniel <singer.daniel85@gmail.com>
2024-08-14 06:44:05 +0200

    update course modal added

--- src/forms/course/CourseForm.tsx
+++ src/forms/course/CourseForm.tsx
@@ -1,4 +1,4 @@
-import { Stack, TextInput } from '@mantine/core';
+import { Checkbox, Stack, TextInput } from '@mantine/core';
 import { CourseFormProvider, useCourseForm } from './context';
 import TutorInput from './TutorInput';
 import SubmitButton from '../../components/buttons/SubmitButton';
@@ -6,24 +6,13 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createCourse } from '../../queries/courses/createCourse';
 import { useModalContext } from '../../context/ModalContext';
 import { showNotification } from '../../helpers/notifications/showNotification';
+import { useCourseActions } from '../../hooks/course/useCourseActions';
+import { useEffect, useMemo } from 'react';
 
 const CourseForm = () => {
   const { toggleModal } = useModalContext();
   const queryClient = useQueryClient();
-  const form = useCourseForm({
-    initialValues: {
-      code: '',
-      title: '',
-      tutor_id: '',
-    },
-    validate: {
-      code: (value) =>
-        !value || value === '' ? 'Kurs Code erforderlich' : null,
-      title: (value) =>
-        !value || value === '' ? 'Kurs Bezeichnung erforderlich' : null,
-    },
-  });
-
+  const { course, isSuccess } = useCourseActions();
   const { mutate: addCourse } = useMutation({
     mutationFn: createCourse,
     onSuccess: (course) => {
@@ -39,6 +28,32 @@ const CourseForm = () => {
       });
     },
   });
+
+  const form = useCourseForm({
+    initialValues: {
+      code: '',
+      title: '',
+      tutor_id: '',
+      active: true,
+    },
+    validate: {
+      code: (value) =>
+        !value || value === '' ? 'Kurs Code erforderlich' : null,
+      title: (value) =>
+        !value || value === '' ? 'Kurs Bezeichnung erforderlich' : null,
+    },
+  });
+
+  useEffect(() => {
+    if (course && isSuccess) {
+      const { code, title, tutor } = course;
+      form.setValues({ code, title, tutor_id: String(tutor.id) });
+      form.resetDirty();
+    }
+  }, [course, isSuccess]);
+
+  const isUpdate = useMemo(() => !!course && !!course.id, [course]);
+
   return (
     <CourseFormProvider form={form}>
       <form onSubmit={form.onSubmit((values) => addCourse(values))}>
@@ -55,7 +70,13 @@ const CourseForm = () => {
             withAsterisk
           />
           <TutorInput />
-          <SubmitButton>Speichern</SubmitButton>
+          <Checkbox
+            label="Kurs aktiv"
+            {...form.getInputProps('active', { type: 'checkbox' })}
+          />
+          <SubmitButton disabled={!form.isDirty() && isUpdate}>
+            {isUpdate ? 'Update' : 'Speichern'}
+          </SubmitButton>
         </Stack>
       </form>
     </CourseFormProvider>
