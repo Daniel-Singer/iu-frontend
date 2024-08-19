a2f69ecedb9ffd89b786411813ce7cc004471adc
Daniel <singer.daniel85@gmail.com>
2024-08-19 11:14:12 +0200

    select of courses for dropdown depends on user role

--- src/forms/issue/selects/CourseSelect.tsx
+++ src/forms/issue/selects/CourseSelect.tsx
@@ -2,16 +2,29 @@ import { Select } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import { listCourses } from '../../../queries/courses/listCourses';
 import { useIssueFormContext } from '../context';
+import { useAuthContext } from '../../../context/AuthContext';
 
 const CourseSelect = () => {
+  const { auth } = useAuthContext();
   const { data: courses } = useQuery({
     queryKey: ['courses'],
     queryFn: listCourses,
     select: (data) => {
-      return data?.map(({ id, code, title }) => ({
-        value: String(id!),
-        label: `${code} - ${title}`,
-      }));
+      if (auth.role !== 'admin') {
+        // show only active courses
+        return data
+          ?.filter(({ active }) => active)
+          .map(({ id, code, title }) => ({
+            value: String(id!),
+            label: `${code} - ${title}`,
+          }));
+      } else {
+        // show all courses if user is admin
+        return data?.map(({ id, code, title }) => ({
+          value: String(id!),
+          label: `${code} - ${title}`,
+        }));
+      }
     },
   });
 
