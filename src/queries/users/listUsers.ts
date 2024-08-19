c41f10c8a78ce7600361e3fdb1719e91034f73b9
Daniel <singer.daniel85@gmail.com>
2024-08-19 18:35:10 +0200

    listUser.ts return type changed

--- src/queries/users/listUsers.ts
+++ src/queries/users/listUsers.ts
@@ -1,6 +1,6 @@
 import axios from '../../axios';
 
-export const listUsers = async (role?: string): Promise<IStudent[]> => {
+export const listUsers = async (role?: string): Promise<IUserReceive[]> => {
   const queryString = role && role !== '' ? `?role=${role}` : '';
   const { data } = await axios.get(`/api/v1/users${queryString}`);
   return data;
