186ccdbf3336c6f4dd5c1ea824859824e260acf5
Daniel <singer.daniel85@gmail.com>
2024-08-27 10:48:27 +0200

    media description can be uploaded

--- src/queries/media/addMediaDescription.ts
+++ src/queries/media/addMediaDescription.ts
@@ -3,6 +3,9 @@ import axios from '../../axios';
 export const addMediaDescription = async (
   values: IIssueMediaBase & { issue_id: string | undefined }
 ): Promise<any> => {
-  const { data } = await axios.post(`/api/v1/media/description`, values);
+  const { data } = await axios.post(
+    `/api/v1/media/description/${values.issue_id}`,
+    values
+  );
   return data;
 };
