c935eac769dc4c8f1c73582abb5478afe8ac67de
Daniel <singer.daniel85@gmail.com>
2024-09-02 11:10:54 +0200

    added filter to show only assinged to tutor issues

--- src/types/Issue.d.ts
+++ src/types/Issue.d.ts
@@ -21,6 +21,7 @@ interface IIssueReceive extends IIssueBase {
   course: ICourseReceive;
   category: ICategoryBase;
   created_from: Pick<IUserBase, 'id' | 'first_name' | 'last_name'>;
+  assigned_to: Pick<IUserBase, 'id' | 'first_name' | 'last_name'>;
   created_at: Date;
   updated_at: Date;
 }
