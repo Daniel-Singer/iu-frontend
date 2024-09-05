3c0d82c2e71ce4a3d7ca12d806a07557c68dbb6c
Daniel <singer.daniel85@gmail.com>
2024-09-05 19:52:34 +0200

    mediatype select depending on category

--- src/forms/issue/selects/MediaSelect.tsx
+++ src/forms/issue/selects/MediaSelect.tsx
@@ -1,9 +1,19 @@
 import { Select } from '@mantine/core';
 import { useIssueFormContext } from '../context';
 import { mediaTypes } from '../../../constants/media';
+import { useMemo } from 'react';
 
 const MediaSelect = () => {
   const form = useIssueFormContext();
+  const filteredMediaTypes = useMemo(() => {
+    if (form.values.category_id === '5') {
+      return mediaTypes.filter(({ value }) => value === 'video');
+    } else if (form.values.category_id === '3') {
+      return mediaTypes.filter(({ value }) => value === 'audio');
+    } else {
+      return mediaTypes;
+    }
+  }, [form.values.category_id]);
   const handleChange = (value: string | undefined) => {
     for (const key in form.values.issue_media) {
       if (key !== 'media_type') {
@@ -16,7 +26,7 @@ const MediaSelect = () => {
   return (
     <>
       <Select
-        data={mediaTypes.map(({ value, label }) => ({ value, label }))}
+        data={filteredMediaTypes.map(({ value, label }) => ({ value, label }))}
         label="Medientyp"
         value={form.values.issue_media.media_type}
         onChange={(value) => handleChange(value!)}
